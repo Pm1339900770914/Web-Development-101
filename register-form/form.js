e7ca17e61139d1adb099ed2341fdcf5962dd1b80
Nutthaphum Uppamai <nutthaphum.uppamai@sony.com>
2026-02-18 15:07:14 +0700

    dom in form

---
+++ register-form/form.js
@@ -0,0 +1,7 @@
+function submitData() {
+    let firstNameDom = document.querySelector("input[name='firstname']")
+    let lastNameDom = document.querySelector("input[name='lastname']")
+    let ageDom = document.querySelector("input[name='age']")
+
+    
+}
